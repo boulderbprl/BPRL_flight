e07e8ac3de1b9ef08678fe142c6e9e5a047561fc
Josh Taylor <josh.taylor@colorado.edu>
2026-07-20 10:43:02 -0600

    Minor changes to teensy filter, upped filter rate to 3 kHz to get a nyquist of 1.5 kHz

--- Teensy/4channel_teensy_externalADC_i2c/4channel_teensy_externalADC_i2c.ino
+++ Teensy/4channel_teensy_externalADC_i2c/4channel_teensy_externalADC_i2c.ino
@@ -2,11 +2,11 @@
 #include <math.h>
 
 #define I2C_SLAVE_ADDR  0x11
-#define SAMPLE_INTERVAL_US  667   // 1.5 kHz sampling (filter design rate)
+#define SAMPLE_INTERVAL_US  333   // 3 kHz sampling (filter design rate)
 #define PRINT_INTERVAL_US  25000  // 40 Hz serial print rate
 
 #define FILTER_FC_HZ  10.0
-#define FILTER_FS_HZ  1500.0
+#define FILTER_FS_HZ  3000.0
 
 #define PIN_CS     10
 #define PIN_RST    11
@@ -138,7 +138,7 @@ void loop() {
 
     digitalWrite(PIN_CS, HIGH);
 
-    // Filter each channel: 4th-order Butterworth LPF, fc = 15 Hz, fs = 1.5 kHz
+    // Filter each channel: 4th-order Butterworth LPF, fc = 15 Hz, fs = 3 kHz
     int16_t filtered[4];
     for (int i = 0; i < 4; i++) {
       double y = biquadProcess(stage1[i], (double)tmp[i]);
