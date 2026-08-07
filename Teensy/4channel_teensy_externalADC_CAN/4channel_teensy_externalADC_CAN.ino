882decd8327640499ec5255f0a2ded5d4e1b7f04
jct418 <josh.taylor@colorado.edu>
2026-08-07 12:25:17 -0600

    Added a linear-regresssion fit for jerk to the logger

--- Teensy/4channel_teensy_externalADC_CAN/4channel_teensy_externalADC_CAN.ino
+++ Teensy/4channel_teensy_externalADC_CAN/4channel_teensy_externalADC_CAN.ino
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <string.h>
 
-const bool LPF_ENABLED = true;  // set false to bypass the Butterworth LPF
+const bool LPF_ENABLED = false;  // set false to bypass the Butterworth LPF
 
 #define CAN_BAUD_RATE  1000000
 #define CAN_TX_ID      0x69      // 8-byte frame: ch1,ch2,ch3,ch4 as int16, little-endian
